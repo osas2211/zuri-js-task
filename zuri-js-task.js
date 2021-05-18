be230bce5587bb40a7796478821ecfe733d7e20f
osaretin <osaretin.frank10@gmail.com>
2021-05-18 01:09:21 +0100

    Prepared for submission

--- zuri-js-task.js
+++ zuri-js-task.js
@@ -2,13 +2,11 @@
 
 function convertFahrToCelsius(Fahr){
     if (typeof(Number(Fahr)) !== "number" || Number.isNaN(Number(Fahr))){
-        console.log(`${Fahr} is not a valid number but a/an ${typeof(Fahr)}`)
         return `${Fahr} is not a valid number but a/an ${typeof(Fahr)}`    
     }
     else{
         Fahr = Number(Fahr);
         result = (((5*Fahr) - 160)/9).toFixed(4);
-        console.log(result);
         return result;
     }   
 }
@@ -18,35 +16,41 @@ function convertFahrToCelsius(Fahr){
 // checkYuGiOh ------ Task 2
 
 function checkYuGiOh(n){
-    let result = []
-    for (let count = 1; count <= n; count += 1){
-        if (count % 2 == 0 && count % 3 == 0 && count % 5 ==0){
-            result.push("yu-gi-oh");
-        }
-        else if (count % 2 == 0 && count % 3 == 0){
-            result.push("yu-gi");
-        }
-        else if (count % 2 == 0 && count % 5 == 0){
-            result.push("yu-oh");
-        }
-        else if (count % 3 == 0 && count % 5 == 0){
-            result.push("gi-oh");
-        }
-        else if (count % 2 == 0){
-            result.push("yu");
-        }
-        else if (count % 3 == 0){
-            result.push("gi");
-        }
-        else if (count % 5 == 0){
-            result.push("oh");
-        }
-        else{
-            result.push(count);
-        }
+    if (typeof(Number(n)) !== "number" || Number.isNaN(Number(n))){
+        return `invalid parameter: "${n}"`
     }
-    console.log(result)
-    return result;
+    else {
+        let result = []
+        n = Number(n)
+        for (let count = 1; count <= n; count += 1){
+            if (count % 2 == 0 && count % 3 == 0 && count % 5 ==0){
+                result.push("yu-gi-oh");
+            }
+            else if (count % 2 == 0 && count % 3 == 0){
+                result.push("yu-gi");
+            }
+            else if (count % 2 == 0 && count % 5 == 0){
+                result.push("yu-oh");
+            }
+            else if (count % 3 == 0 && count % 5 == 0){
+                result.push("gi-oh");
+            }
+            else if (count % 2 == 0){
+                result.push("yu");
+            }
+            else if (count % 3 == 0){
+                result.push("gi");
+            }
+            else if (count % 5 == 0){
+                result.push("oh");
+            }
+            else{
+                result.push(count);
+            }
+        }
+        return result;
+    }
+    
 }
 
 
